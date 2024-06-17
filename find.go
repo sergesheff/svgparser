f4b3e0f9396513c7664ee6712444c2cd4d2bbebc
Sergey Shevchuk <sergesheff84@gmail.com>
2024-06-17 17:04:34 +0300

    added new method for searching by a charData

--- find.go
+++ find.go
@@ -1,5 +1,7 @@
 package svgparser
 
+import "strings"
+
 // FindID finds the first child with the specified ID.
 func (e *Element) FindID(id string) *Element {
 	for _, child := range e.Children {
@@ -21,6 +23,28 @@ func (e *Element) FindAll(name string) []*Element {
 			elements = append(elements, child)
 		}
 		elements = append(elements, child.FindAll(name)...)
+	}
+	return elements
+}
+
+// FindByCharData finds all children with the given chardata.
+func (e *Element) FindByCharData(text string) []*Element {
+	if e == nil {
+		return nil
+	}
+
+	text = strings.ToLower(text)
+
+	var elements []*Element
+	for _, child := range e.Children {
+		if child == nil {
+			continue
+		}
+
+		if strings.Contains(strings.ToLower(child.Content), text) {
+			elements = append(elements, child)
+		}
+		elements = append(elements, child.FindByCharData(text)...)
 	}
 	return elements
 }
